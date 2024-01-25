596cce4cff2178e28b3ad7dce65da4c777fa81a8
Андрей Серебренников <a.serebrennikov1986@gmail.com>
2024-01-25 17:02:41 +0500

    fix package-lock.json package.json src/cart.js tests/cart.test.js

--- src/cart.js
+++ src/cart.js
@@ -1,23 +1,17 @@
+export const makeCart = () => [];
 
-export const makeCart = () => {
-    return [];
-};
 export const addItem = (product, count) => {
   const key = product.name;
-  return {[key]:product, count};
+  return { [key]: product, count };
 };
 
 export const getCost = (data) => {
   let sum = 0;
   for (const product of data) {
-    const [key, ...rest] = Object.keys(product);
+    const [key] = Object.keys(product);
     sum += product.count * product[key].price;
   }
   return sum;
 };
 
-export const getCount = (data) => {
-  return data.reduce((acc, product) => {
-    return acc + product.count;
-  }, 0);
-};
+export const getCount = (data) => data.reduce((acc, product) => acc + product.count, 0);
